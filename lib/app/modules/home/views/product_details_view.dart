ab63ee736f779bcdf22ed9d8d4bb23ece3e7f922
Pragna Bhatt <94222746+PragnaBhatt@users.noreply.github.com>
2022-09-29 18:17:13 +0530

    detail page data

--- lib/app/modules/home/views/product_details_view.dart
+++ lib/app/modules/home/views/product_details_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:therapeutic/app/constants/size_constants.dart';
 import 'package:therapeutic/app/modules/home/controllers/home_controller.dart';
 
+import '../../../constants/color_constants.dart';
 import '../../models/product_detail_model.dart';
 import 'package:flutter_html/flutter_html.dart';
 
@@ -12,7 +14,8 @@ class ProductDetailView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     String prodId = Get.arguments;
     Future<ProductDetailModel> data = controller.fetchFoodProductDetail(prodId);
-
+    RxString dataToDisplay = "".obs;
+    RxInt activeIndex = 100.obs;
     return Scaffold(
       appBar: AppBar(
         title: Text("Get.arguments!!"),
@@ -26,21 +29,153 @@ class ProductDetailView extends GetView<HomeController> {
                 ConnectionState.waiting) {
               return Center(child: const CircularProgressIndicator());
             } else if (prodDetailSnap.hasData) {
+              dataToDisplay.value = prodDetailSnap.data!.data!.description!;
+              activeIndex.value = 0;
               return SingleChildScrollView(
                   child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.network(
-                      "https://png.pngtree.com/png-vector/20210929/ourmid/pngtree-404-not-found-mdern-transparent-background-png-image_3963794.png",
-                      height: 100,
-                      width: 200,
-                    ),
-                    Text(prodDetailSnap.data!.data!.name!),
-                    Html(data:prodDetailSnap.data!.data!.description! ),
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Obx(() {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.network(
+                            "https://png.pngtree.com/png-vector/20210929/ourmid/pngtree-404-not-found-mdern-transparent-background-png-image_3963794.png",
+                            height: 250,
+                            width: 250,
+                          ),
+                          Text(
+                            prodDetailSnap.data!.data!.name!,
+                            style:
+                                TextStyle(fontSize: SizeConstants.FONT_SIZE_16),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  dataToDisplay.value =
+                                      prodDetailSnap.data!.data!.description!;
 
-                  ],
-                ),
+                                  activeIndex.value = 0;
+                                },
+                                child: Chip(
+                                    elevation: 1,
+                                    backgroundColor: activeIndex.value == 0
+                                        ? ColorConstants.greeen
+                                        : ColorConstants.colorPrimary,
+                                    label: Text("Description",
+                                        style: TextStyle(color: Colors.white))),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  activeIndex.value = 1;
+                                  dataToDisplay.value = prodDetailSnap
+                                      .data!.data!.nutritionalBenefits!;
+                                },
+                                child: Chip(
+                                    elevation: 1,
+                                    backgroundColor: activeIndex.value == 1
+                                        ? ColorConstants.greeen
+                                        : ColorConstants.colorPrimary,
+                                    label: Text("NutritionalBenefits",
+                                        style: TextStyle(color: Colors.white))),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  activeIndex.value = 2;
+                                  dataToDisplay.value = prodDetailSnap
+                                      .data!.data!.healthBenefits!;
+                                },
+                                child: Chip(
+                                    elevation: 1,
+                                    backgroundColor: activeIndex.value == 2
+                                        ? ColorConstants.greeen
+                                        : ColorConstants.colorPrimary,
+                                    label: Text("HealthBenefits",
+                                        style: TextStyle(color: Colors.white))),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  activeIndex.value = 3;
+                                  dataToDisplay.value = prodDetailSnap
+                                      .data!.data!.consumptionTips!;
+                                },
+                                child: Chip(
+                                    elevation: 1,
+                                    backgroundColor: activeIndex.value == 3
+                                        ? ColorConstants.greeen
+                                        : ColorConstants.colorPrimary,
+                                    label: Text("ConsumptionTips",
+                                        style: TextStyle(color: Colors.white))),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  activeIndex.value = 4;
+                                  dataToDisplay.value =
+                                      prodDetailSnap.data!.data!.caution!;
+                                },
+                                child: Chip(
+                                    elevation: 1,
+                                    backgroundColor: activeIndex.value == 4
+                                        ? ColorConstants.greeen
+                                        : ColorConstants.colorPrimary,
+                                    label: Text("Caution",
+                                        style: TextStyle(color: Colors.white))),
+                              ),
+                            ],
+                          ),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+
+                                TextButton.icon(onPressed: (){}, icon:  Icon(
+                                  Icons.note_alt_outlined,
+                                  color: ColorConstants.colorPrimary1,
+                                ), label:  Text(
+                                  "Add Note",
+                                  style: TextStyle(color: ColorConstants.colorPrimary1, fontSize: SizeConstants.FONT_SIZE),
+                                ))
+
+                               ,
+                               /* SizedBox(
+                                  width: 10,
+                                ),*/
+
+                                TextButton.icon(
+
+
+                                    onPressed: (){}, icon:  Icon(
+                                  Icons.star_border,
+                                  color: ColorConstants.colorPrimary1,
+                                ), label:  Text(
+
+                                  "Add to Favorite",
+
+                                  style: TextStyle(color: ColorConstants.colorPrimary1, fontSize: SizeConstants.FONT_SIZE),
+                                ))
+
+                              ]),
+                          Html(
+                            data: dataToDisplay.value,
+                            style: {
+                              "body": Style(
+                                fontSize: FontSize(SizeConstants.FONT_SIZE),
+                                fontWeight: FontWeight.normal,
+                              ),
+                            },
+                          ),
+                        ],
+                      );
+                    })),
               ));
             } else {
               return Text("no data found");
--- lib/app/modules/home/views/widgets/item_widget.dart
+++ lib/app/modules/home/views/widgets/item_widget.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:get/get.dart';
 import 'package:therapeutic/app/constants/color_constants.dart';
 import 'package:therapeutic/app/modules/models/product_model.dart'
     as productModel;
+import 'package:therapeutic/app/routes/app_pages.dart';//odel;
 
 class ItemWidget extends StatelessWidget {
   productModel.Data data;
@@ -16,47 +18,77 @@ class ItemWidget extends StatelessWidget {
     str = str.substring(0, 430) + "...";
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Card(
-        elevation: 2,
-        child: ListTile(
-          title: Text(data!.type!),
+      child: InkWell(
+        onTap: ((){Get.toNamed(Routes.PRODUCT_DETAIL,arguments: data!.sId!);}),
+        child: Card(
+         // elevation: 1,
+          child: ListTile(
+            title: Text(data!.name!),
+            subtitle: Column(
+              // mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
 
-          subtitle: Column(
-           // mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
+                Html(data: str, shrinkWrap: true),
+                InkWell(
+                  onTap: ((){Get.toNamed(Routes.PRODUCT_LIST,arguments: data!.type!);}),
+                  child: Chip(
 
-          //    Text(data!.name!,style: TextStyle(color: Colors.black,),textAlign: TextAlign.right,),
+                    elevation: 1,
+                      backgroundColor: ColorConstants. colorPrimaryDark,
+                      label:
+                          Text(data!.type!, style: TextStyle(color: Colors.white))),
 
-              Html(data: str, shrinkWrap: true),
+                ),
 
-              Container(
-                child: Text(data!.name!,style: TextStyle(color: Colors.black,backgroundColor: Colors.deepOrange),),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0,right: 8.0,top: 8.0,bottom: 8.0),
-                child: Divider(height: 1,color: ColorConstants.color_black,),
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 8.0, right: 8.0, top: 8.0, bottom: 8.0),
+                  child: Divider(
+                    height: 1,
+                    color: ColorConstants.color_black,
+                  ),
+                ),
 
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Icon(Icons.note_alt_outlined,color: Colors.black,),
-                    Text("Add Note",style: TextStyle(color: Colors.black),),
-                SizedBox(width: 10,),
-                    Icon(Icons.star_border,color: Colors.black,),
-                    Text("Add to Favorite",style: TextStyle(color: Colors.black),),
-                SizedBox(width: 10,),
-
-                Icon(Icons.remove_red_eye,color: Colors.black,),
-                Text(" ${data!.noOfViews!.toString()}",style: TextStyle(color: Colors.black),)
-              ])
-            ],
-          ),
-          leading: Icon(
-            Icons.account_tree_sharp,
-            color: Colors.deepOrange,
-            size: 40,
+                Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+                  Icon(
+                    Icons.note_alt_outlined,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    "Add Note",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    Icons.star_border,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    "Add to Favorite",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    Icons.remove_red_eye,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    " ${data!.noOfViews!.toString()}",
+                    style: TextStyle(color: Colors.black),
+                  )
+                ])
+              ],
+            ),
+            leading: Icon(
+              Icons.account_tree_sharp,
+              color: ColorConstants.colorPrimaryDark,
+              size: 40,
+            ),
           ),
         ),
       ),
